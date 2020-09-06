0540d4fd397e23c51b4dd23359976a3a2ac695bd
Vasilevigvl <66677463+Vasilevigvl@users.noreply.github.com>
2020-09-06 22:39:33 +0300

    Update ops.js

--- js/ops.js
+++ js/ops.js
@@ -138,19 +138,3 @@ $("[data-scroll-to]").click(e => {
 });
 
 
-$(".wrapper").on("touchmove", e => e.preventDefault());
-
-
-$(function() {
-  $("body").swipe ({
-    swipe: function (event, direction)  {
-      const scroller = viewportScroller();
-      let scrollDirection = "";
-
-      if (direction === "up") scrollDirection = "next";
-      if (direction === "down") scrollDirection = "prev";
-
-      scroller[scrollDirection]();
-    },
-  });
-)};
